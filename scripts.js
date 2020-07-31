8c7547f4c34ece496572abe94f1e2d2542a3ad48
Lacey Savage <lacey.savage@gmail.com>
2020-07-31 14:44:54 -0400

    added play/pause icons and functions

--- scripts.js
+++ scripts.js
@@ -6,5 +6,25 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
 //Build our functions
+function togglePlay() {
+    // if(video.paused) {
+    //     video.play();
+    // } else {
+    //     video.pause();
+    // }
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton() {
+    const icon = this.paused ? "►" : "❚ ❚";
+    toggle.textContent = icon;
+}
+
 //Hook up the event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
